c084f83b69cac8025fe2e16f378bec3e490ea46f
Destry A <destry.amiott@gmail.com>
2017-05-14 21:28:47 -0500

    messed with lots of stuff

---
+++ js/frameScripts.js
@@ -0,0 +1,9 @@
+/**
+ * Created by destr on 5/14/2017.
+ */
+
+    function changeto(url1, url2) {
+        parent.document.getElementById("frame").src = url1
+        parent.document.getElementById("subFrame").src = url2
+    }
+
