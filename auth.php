aefdfc03df6902c3d1f04a3b2943eb1cd83a146f
Destry A <destry.amiott@gmail.com>
2017-05-21 02:35:47 -0500

    fixed design, added more mobile support.

--- auth.php
+++ auth.php
@@ -1,7 +1,14 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: destr
+ * User: Destry Amiott
  * Date: 5/20/2017
  * Time: 1:52 PM
  */
+
+
+    if($_POST['password'] == 'e3fca7865a7ef536a43aee19730e73ba'){
+        header("Location: http://kanoka42.github.io/units.html"); /* Redirect here if the password is correct */
+    } else {
+        header("Location: http://www.google.com"); /* Redirect here if the password is correct */
+    }
